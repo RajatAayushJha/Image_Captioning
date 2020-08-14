4ff3a1151fe2ec19e545623f195bec1fcdd14b8a
RajatAayushJha <43760351+RajatAayushJha@users.noreply.github.com>
2020-08-14 15:37:32 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,17 @@
+import SceneDesc
+
+import sys
+
+def train(epoch):
+    sd = SceneDesc.scenedesc()
+    model = sd.create_model()
+    batch_size = 512
+    d=sd.data_process(4
+    	)
+    #model.fit(sd.data_process(4))
+    model.fit_generator(sd.data_process(batch_size=batch_size), steps_per_epoch=sd.no_samples/batch_size, epochs=epoch, verbose=2, callbacks=None)
+    model.save('Output/Model.h5', overwrite=True)
+    model.save_weights('Output/Weights.h5',overwrite=True)
+ 
+if __name__=="__main__":
+    train(int(sys.argv[1]))
